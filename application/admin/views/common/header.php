aeecb3ea27cffa334c686a6cff0c83b947dfac68
xpcode <4019980@qq.com>
2014-03-02 21:33:38 +0800

    delete trash

--- application/admin/views/common/header.php
+++ application/admin/views/common/header.php
@@ -19,20 +19,6 @@
 	</style>
 	<link href="/admin/css/bootstrap-responsive.css" rel="stylesheet">
 	<link href="/admin/css/charisma-app.css" rel="stylesheet">
-	<link href="/admin/css/jquery-ui-1.8.21.custom.css" rel="stylesheet">
-	<link href='/admin/css/fullcalendar.css' rel='stylesheet'>
-	<link href='/admin/css/fullcalendar.print.css' rel='stylesheet'  media='print'>
-	<link href='/admin/css/chosen.css' rel='stylesheet'>
-	<link href='/admin/css/uniform.default.css' rel='stylesheet'>
-	<link href='/admin/css/colorbox.css' rel='stylesheet'>
-	<link href='/admin/css/jquery.cleditor.css' rel='stylesheet'>
-	<link href='/admin/css/jquery.noty.css' rel='stylesheet'>
-	<link href='/admin/css/noty_theme_default.css' rel='stylesheet'>
-	<link href='/admin/css/elfinder.min.css' rel='stylesheet'>
-	<link href='/admin/css/elfinder.theme.css' rel='stylesheet'>
-	<link href='/admin/css/jquery.iphone.toggle.css' rel='stylesheet'>
-	<link href='/admin/css/opa-icons.css' rel='stylesheet'>
-	<link href='/admin/css/uploadify.css' rel='stylesheet'>
 
 	<!-- The HTML5 shim, for IE6-8 support of HTML5 elements -->
 	<!--[if lt IE 9]>
